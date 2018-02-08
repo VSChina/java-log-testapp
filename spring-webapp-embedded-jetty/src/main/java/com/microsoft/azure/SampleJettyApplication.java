4dce34dae2f0523527c04da95847a312744e8477
weiping <wepa@microsoft.com>
2018-02-08 11:56:47 +0800

    add function

--- spring-webapp-embedded-jetty/src/main/java/com/microsoft/azure/SampleJettyApplication.java
+++ spring-webapp-embedded-jetty/src/main/java/com/microsoft/azure/SampleJettyApplication.java
@@ -16,14 +16,34 @@
 
 package com.microsoft.azure;
 
+import com.microsoft.azure.serverless.functions.ExecutionContext;
+import com.microsoft.azure.serverless.functions.HttpRequestMessage;
+import com.microsoft.azure.serverless.functions.HttpResponseMessage;
+import com.microsoft.azure.serverless.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.serverless.functions.annotation.FunctionName;
+import com.microsoft.azure.serverless.functions.annotation.HttpTrigger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class SampleJettyApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SampleJettyApplication.class);
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(SampleJettyApplication.class, args);
 	}
 
+	@FunctionName("hello")
+	public HttpResponseMessage<String> hello(
+			@HttpTrigger(name = "req", methods = {"get", "post"}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+			final ExecutionContext context) {
+		LOGGER.info("java http trigger processed a request");
+		LOGGER.trace("[Trace log]java http trigger processed a request");
+
+		return request.createResponse(200, "Hello World from Azure Function!");
+	}
 }
